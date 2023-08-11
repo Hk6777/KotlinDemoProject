6d225fd49bb8c67b86c9c7a0b64dfd111a1fcb67
Hk6777 <100499777+Hk6777@users.noreply.github.com>
2023-08-11 19:13:06 -0400

    Listview Onclick Event and oepn new Screen

--- app/src/main/java/com/example/kotlindemo/JetPackComposed/Demo.kt
+++ app/src/main/java/com/example/kotlindemo/JetPackComposed/Demo.kt
@@ -3,15 +3,24 @@ package com.example.kotlindemo.JetPackComposed
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -21,8 +30,7 @@ import androidx.navigation.compose.rememberNavController
 import com.example.kotlindemo.JetPackComposed.Activity.CityList
 import com.example.kotlindemo.JetPackComposed.Activity.ListviewScreen
 import com.example.kotlindemo.JetPackComposed.ui.theme.KotlinDemoTheme
-import com.example.kotlindemo.JetPackComposed.ui.theme.Shapes
-import com.example.kotlindemo.JetPackComposed.ui.theme.md_theme_light_surfaceTint
+import com.example.kotlindemo.JetPackComposed.ui.theme.md_theme_light_errorContainer
 
 class Demo : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,43 +45,88 @@ class Demo : ComponentActivity() {
 }
 
 @Composable
-fun FirstScreen(navController: NavController) {
+fun FirstScreen(navController: NavController, exapleList: List<String>) {
     Column(
         modifier = Modifier
             .padding(16.dp)
             .fillMaxSize()
     ) {
-        Button(
-            onClick = {
-                // Navigate to the second screen when the button is clicked
-                navController.navigate("ListviewScreen")
-            },
-            modifier = Modifier
-                .padding(8.dp)
-                .fillMaxWidth(),
-            colors = ButtonDefaults.buttonColors(md_theme_light_surfaceTint),
-            shape = Shapes.extraSmall
+//        Button(
+//            onClick = {
+//                // Navigate to the second screen when the button is clicked
+//                navController.navigate("ListviewScreen")
+//            },
+//            modifier = Modifier
+//                .padding(8.dp)
+//                .fillMaxWidth(),
+//            colors = ButtonDefaults.buttonColors(md_theme_light_surfaceTint),
+//            shape = Shapes.extraSmall
+//
+//        ) {
+//            Text("Listview & GridView Demo")
+//        }
+//        Button(
+//            onClick = {
+//                // Navigate to the second screen when the button is clicked
+//                navController.navigate("citylist")
+//            },
+//            modifier = Modifier
+//                .padding(8.dp)
+//                .fillMaxWidth(),
+//            colors = ButtonDefaults.buttonColors(md_theme_light_surfaceTint),
+//            shape = Shapes.extraSmall
+//
+//        ) {
+//            Text("Citylist")
+//        }
 
-        ) {
-            Text("Listview & GridView Demo")
-        }
-        Button(
-            onClick = {
-                // Navigate to the second screen when the button is clicked
-                navController.navigate("citylist")
-            },
-            modifier = Modifier
-                .padding(8.dp)
-                .fillMaxWidth(),
-            colors = ButtonDefaults.buttonColors(md_theme_light_surfaceTint),
-            shape = Shapes.extraSmall
-
-        ) {
-            Text("Citylist")
+        LazyColumn {
+            items(exapleList.size) { index ->
+                ClassRow(
+                    exapleList[index]
+                )
+                { navController.navigate("ExapleList/${exapleList[index]}") }
+            }
         }
     }
 }
 
+@Composable
+fun ClassRow(exapleList: String, onItemClick: () -> Unit) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp)
+            .clickable { onItemClick() },
+        horizontalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+
+        ClassList(exapleList)
+    }
+}
+
+@Composable
+fun ClassList(cityName: String) {
+    Card(
+        modifier = Modifier
+            .height(50.dp)
+    ) {
+
+        Box(
+
+            modifier = Modifier
+                .background(md_theme_light_errorContainer, RoundedCornerShape(8.dp)),
+
+            ) {
+            Text(
+                text = cityName,
+                style = MaterialTheme.typography.titleSmall,
+                modifier = Modifier.padding(16.dp),
+                color = Color.Black
+            )
+        }
+    }
+}
 
 
 @Composable
@@ -81,9 +134,22 @@ fun App() {
     val navController = rememberNavController()
 
     NavHost(navController, startDestination = "firstScreen") {
-        composable("firstScreen") { FirstScreen(navController) }
-        composable("ListviewScreen") { ListviewScreen() }
-        composable("citylist") {
+        composable("firstScreen") {
+            var exapleList = listOf(
+                "ListView & Gridview Demo",
+                "Citylist Coustom Grid",
+            )
+            FirstScreen(
+                navController,
+                exapleList
+                // Add more cities here
+            )
+
+
+        }
+//        composable("ListviewScreen") { ListviewScreen() }
+        composable("ExapleList/ListView & Gridview Demo") { ListviewScreen() }
+        composable("ExapleList/Citylist Coustom Grid") {
             val cities = listOf(
                 "New York",
                 "Los Angeles",
@@ -94,7 +160,8 @@ fun App() {
                 "Miami",
                 // Add more cities here
             )
-            CityList(cities) }
+            CityList(cities)
+        }
     }
 }
 
