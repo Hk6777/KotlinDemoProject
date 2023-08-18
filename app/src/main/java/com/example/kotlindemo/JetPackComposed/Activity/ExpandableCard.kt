4419470f6782a874dc85a50f7d4cb446359ad6b3
Hk6777 <100499777+Hk6777@users.noreply.github.com>
2023-08-18 10:27:48 -0400

    Card Design (Jetpack)

--- app/src/main/java/com/example/kotlindemo/JetPackComposed/Activity/ExpandableCard.kt
+++ app/src/main/java/com/example/kotlindemo/JetPackComposed/Activity/ExpandableCard.kt
@@ -27,6 +27,7 @@ import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
--- app/src/main/java/com/example/kotlindemo/JetPackComposed/Activity/MatrialDesignDemos.kt
+++ app/src/main/java/com/example/kotlindemo/JetPackComposed/Activity/MatrialDesignDemos.kt
@@ -1,12 +1,16 @@
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Check
@@ -23,6 +27,7 @@ import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.BottomAppBarDefaults
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.FloatingActionButtonDefaults
@@ -39,15 +44,22 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.kotlindemo.JetPackComposed.Activity.BottomSheet
 import com.example.kotlindemo.JetPackComposed.ui.theme.Shapes
 import com.example.kotlindemo.JetPackComposed.ui.theme.md_theme_light_surfaceTint
+import com.example.kotlindemo.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Preview
@@ -101,14 +113,92 @@ fun MatrialDesignDemos() {
             Text(text = "Show BottomSheet")
         }
 
-//Costom Button
+        //Costom Button
+        CoustomButton()
+
+        CardView1()
+
+    }
+
+
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CardView1() {
+    Column(Modifier.fillMaxWidth()) {
+
+        Spacer(modifier = Modifier.size(10.dp))
+        Card(
+            onClick = { /*TODO*/ },
+            modifier = Modifier
+                .size(width = 250.dp, height = 180.dp)
+                .align(CenterHorizontally)
+        ) {
+
+            Column(Modifier.padding(10.dp)) {
+
+                Row(
+                    Modifier.fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+
+                    Box(
+                        modifier = Modifier
+                            .size(35.dp)
+                            .clip(RoundedCornerShape(50))
+                            .background(Color.Gray)
+
+
+                    ) {
+                        // Content inside the rounded shape
+                        Text(
+                            text = "H",
+                            modifier = Modifier.align(Alignment.Center),
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 18.sp
+                        )
+//                    Icon(
+//                            Icons.Filled.Star, contentDescription = "", Modifier.align(
+//                                Alignment.Center
+//                            )
+//                        )
+                    }
+                    Column(Modifier.fillMaxWidth()) {
+                        Icon(
+                            Icons.Filled.Star,
+                            contentDescription = "",
+                            Modifier.align(Alignment.End).size(15.dp)
+                        )
+                    }
+
+
+                }
+
+                Spacer(modifier = Modifier.size(10.dp))
+                Text(text = "Title", fontSize = 25.sp)
+                Text(text = "SubTitle", fontSize = 15.sp)
+                Spacer(modifier = Modifier.size(10.dp))
+                Text(text = stringResource(R.string.dummy_text),Modifier.fillMaxWidth(), fontSize = 10.sp, maxLines = 3, lineHeight = 13.sp)
+
+            }
+
+        }
+    }
+
+}
+
+
+@Composable
+fun CoustomButton() {
+    Column(modifier = Modifier.fillMaxWidth()) {
+
         Button(
             onClick = { /*TODO*/ },
             contentPadding = ButtonDefaults.ButtonWithIconContentPadding,
 
             elevation = ButtonDefaults.elevatedButtonElevation(defaultElevation = 20.dp),
-            modifier = Modifier
-                .align(CenterHorizontally),
+            modifier = Modifier.align(CenterHorizontally),
             border = BorderStroke(2.dp, color = Color.LightGray)
 
         ) {
@@ -121,13 +211,8 @@ fun MatrialDesignDemos() {
             Spacer(modifier = Modifier.size(ButtonDefaults.IconSpacing))
             Text(text = "Like")
         }
-
-
     }
-
-
 }
-
 
 //@OptIn(ExperimentalMaterial3Api::class)
 //@Composable
@@ -202,17 +287,13 @@ fun AssistshipView() {
 
 
         Text(text = "2. Assistchip")
-        AssistChip(
-            onClick = { /*TODO*/ },
-            label = { Text(text = "Assist Chip") },
-            leadingIcon = {
-                Icon(
-                    Icons.Filled.Star,
-                    contentDescription = "",
-                    Modifier.size(AssistChipDefaults.IconSize)
-                )
-            },
-            modifier = Modifier.align(CenterHorizontally)
+        AssistChip(onClick = { /*TODO*/ }, label = { Text(text = "Assist Chip") }, leadingIcon = {
+            Icon(
+                Icons.Filled.Star,
+                contentDescription = "",
+                Modifier.size(AssistChipDefaults.IconSize)
+            )
+        }, modifier = Modifier.align(CenterHorizontally)
         )
     }
 }
@@ -222,30 +303,23 @@ fun AssistshipView() {
 fun BedgeView() {
     Text(text = "3. Bedge")
     NavigationBar {
-        NavigationBarItem(
-            selected = false,
-            onClick = { /*TODO*/ },
-            icon = {
-                BadgedBox(
-                    badge = {
-                        Badge {
-                            val badgeNumber = "8"
-                            Text(badgeNumber,
-                                modifier = Modifier.semantics {
-                                    contentDescription = "$badgeNumber new notifications"
-                                })
-
-                        }
-
-                    }) {
-                    Icon(
-                        Icons.Filled.Star,
-                        contentDescription = ""
-                    )
+        NavigationBarItem(selected = false, onClick = { /*TODO*/ }, icon = {
+            BadgedBox(badge = {
+                Badge {
+                    val badgeNumber = "8"
+                    Text(badgeNumber, modifier = Modifier.semantics {
+                        contentDescription = "$badgeNumber new notifications"
+                    })
 
                 }
+
+            }) {
+                Icon(
+                    Icons.Filled.Star, contentDescription = ""
+                )
+
             }
-        )
+        })
 
     }
 }
@@ -253,112 +327,90 @@ fun BedgeView() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun NavigationView() {
-    BottomAppBar(
-        actions = {
-            IconButton(onClick = { /* doSomething() */ }) {
-                Icon(Icons.Filled.Check, contentDescription = "Localized description")
-            }
-            IconButton(onClick = { /* doSomething() */ }) {
-
-
-                Icon(
-                    Icons.Filled.Edit,
-                    contentDescription = "Localized description",
-                )
-            }
-            IconButton(onClick = { /* doSomething() */ }) {
-                Icon(
-                    Icons.Filled.Star,
-                    contentDescription = "Localized description",
-                )
-            }
-            IconButton(onClick = { /* doSomething() */ }) {
-                Icon(
-                    Icons.Filled.Settings,
-                    contentDescription = "Localized description",
-                )
-            }
-
-            IconButton(onClick = { /* doSomething() */ }) {
-                Icon(
-                    Icons.Filled.Favorite,
-                    contentDescription = "Localized description",
-                )
-            }
-            NavigationBarItem(
-                selected = false,
-                onClick = { /*TODO*/ },
-                icon = {
-                    BadgedBox(
-                        badge = {
-                            Badge {
-                                val badgeNumber = "8"
-                                Text(badgeNumber,
-                                    modifier = Modifier.semantics {
-                                        contentDescription = "$badgeNumber new notifications"
-                                    })
-
-                            }
-
-                        }) {
-                        Icon(
-                            Icons.Filled.Star,
-                            contentDescription = ""
-                        )
-
-                    }
-                }
-            )
-        },
-        floatingActionButton = {
-            FloatingActionButton(
-                onClick = { /* do something */ },
-                containerColor = BottomAppBarDefaults.bottomAppBarFabColor,
-                elevation = FloatingActionButtonDefaults.bottomAppBarFabElevation()
-            ) {
-                Icon(Icons.Filled.Add, "Localized description")
-            }
+    BottomAppBar(actions = {
+        IconButton(onClick = { /* doSomething() */ }) {
+            Icon(Icons.Filled.Check, contentDescription = "Localized description")
         }
-    )
+        IconButton(onClick = { /* doSomething() */ }) {
+
+
+            Icon(
+                Icons.Filled.Edit,
+                contentDescription = "Localized description",
+            )
+        }
+        IconButton(onClick = { /* doSomething() */ }) {
+            Icon(
+                Icons.Filled.Star,
+                contentDescription = "Localized description",
+            )
+        }
+        IconButton(onClick = { /* doSomething() */ }) {
+            Icon(
+                Icons.Filled.Settings,
+                contentDescription = "Localized description",
+            )
+        }
+
+        IconButton(onClick = { /* doSomething() */ }) {
+            Icon(
+                Icons.Filled.Favorite,
+                contentDescription = "Localized description",
+            )
+        }
+        NavigationBarItem(selected = false, onClick = { /*TODO*/ }, icon = {
+            BadgedBox(badge = {
+                Badge {
+                    val badgeNumber = "8"
+                    Text(badgeNumber, modifier = Modifier.semantics {
+                        contentDescription = "$badgeNumber new notifications"
+                    })
+
+                }
+
+            }) {
+                Icon(
+                    Icons.Filled.Star, contentDescription = ""
+                )
+
+            }
+        })
+    }, floatingActionButton = {
+        FloatingActionButton(
+            onClick = { /* do something */ },
+            containerColor = BottomAppBarDefaults.bottomAppBarFabColor,
+            elevation = FloatingActionButtonDefaults.bottomAppBarFabElevation()
+        ) {
+            Icon(Icons.Filled.Add, "Localized description")
+        }
+    })
 }
 
 @Composable
 fun AlertDialogComponent(onDismiss: () -> Unit) {
 
-    AlertDialog(
-        onDismissRequest = {
+    AlertDialog(onDismissRequest = {
 
+        onDismiss()
+    }, icon = { Icon(Icons.Filled.Favorite, contentDescription = null) }, title = {
+        Text(text = "Title")
+    }, text = {
+        Text(
+            "This area typically contains the supportive text " + "which presents the details regarding the Dialog's purpose."
+        )
+    }, confirmButton = {
+        TextButton(onClick = {
             onDismiss()
-        },
-        icon = { Icon(Icons.Filled.Favorite, contentDescription = null) },
-        title = {
-            Text(text = "Title")
-        },
-        text = {
-            Text(
-                "This area typically contains the supportive text " +
-                        "which presents the details regarding the Dialog's purpose."
-            )
-        },
-        confirmButton = {
-            TextButton(
-                onClick = {
-                    onDismiss()
-                }
-            ) {
-                Text("Confirm")
-            }
-        },
-        dismissButton = {
-            TextButton(
-                onClick = {
-                    onDismiss()
-                }
-            ) {
-                Text("Dismiss")
-            }
+        }) {
+            Text("Confirm")
         }
-    )
+    }, dismissButton = {
+        TextButton(onClick = {
+            onDismiss()
+        }) {
+            Text("Dismiss")
+        }
+    })
 
 }
 
